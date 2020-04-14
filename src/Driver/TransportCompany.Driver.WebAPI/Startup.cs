2915d50801f77fb1c181205e2572ff0c2b4f4999
Arkadiusz Chudy <achudy333@gmail.com>
2020-04-14 22:43:26 +0200

    Prepare driver api startup

--- src/Driver/TransportCompany.Driver.WebAPI/Startup.cs
+++ src/Driver/TransportCompany.Driver.WebAPI/Startup.cs
@@ -1,51 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using TransportCompany.Driver.Infrastructure.Persistence;
+using TransportCompany.Shared.ApiInfrastructure;
 
 namespace TransportCompany.Driver.WebAPI
 {
-    public class Startup
+    public class Startup: BaseStartup<Startup>
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env) 
+            : base(configuration, env, "Driver")
         {
-            Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        protected override void ConfigureDataContext(IServiceCollection services)
         {
-            services.AddControllers();
-        }
+            services.AddDbContext<DriverDbContext>(options => options
+                .UseSqlServer(ConnectionString, sqlServerOptions =>
+                {
+                    sqlServerOptions.MigrationsAssembly("TransportCompany.Driver.Infrastructure");
+                    sqlServerOptions.EnableRetryOnFailure(5);
+                }));
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            var dbContext = services.BuildServiceProvider().GetService<DriverDbContext>();
+            dbContext.Database.Migrate();
         }
     }
 }
