520bbc89d08a7b2dd4ffd51e6297c8a51d95ed90
Arkadiusz Chudy <achudy333@gmail.com>
2020-04-21 21:32:30 +0200

    Create customer domain events

---
+++ src/Customer/TransportCompany.Customer.Application/Events/DriverRated.cs
@@ -0,0 +1,14 @@
+﻿using TransportCompany.Shared.Domain.Events;
+
+namespace TransportCompany.Customer.Application.Events
+{
+    public class DriverRated : IDomainEvent
+    {
+        public DriverRated(int grade)
+        {
+            Grade = grade;
+        }
+
+        public int Grade { get; }
+    }
+}
---
+++ src/Customer/TransportCompany.Customer.Application/Events/RouteDeleted.cs
@@ -0,0 +1,17 @@
+﻿using TransportCompany.Shared.Domain.Events;
+using TransportCompany.Shared.Domain.ValueObjects;
+
+namespace TransportCompany.Customer.Application.Events
+{
+    public sealed class RouteDeleted : IDomainEvent
+    {
+        public RouteDeleted(Address startPoint, Address destination)
+        {
+            StartPoint = startPoint;
+            Destination = destination;
+        }
+
+        public Address StartPoint { get; }
+        public Address Destination { get; }
+    }
+}
