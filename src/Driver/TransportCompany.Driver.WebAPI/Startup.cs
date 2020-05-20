cb25db37e957e6606e6549eee3f082f6c9f5414a
Arkadiusz Chudy <achudy333@gmail.com>
2020-05-20 22:09:45 +0200

    Adjust driver startup

--- src/Driver/TransportCompany.Driver.WebAPI/Startup.cs
+++ src/Driver/TransportCompany.Driver.WebAPI/Startup.cs
@@ -1,8 +1,12 @@
+using System.Reflection;
+using AutoMapper;
+using MediatR;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TransportCompany.Driver.Infrastructure.Persistence;
+using TransportCompany.Order.Infrastructure.Persistence;
 using TransportCompany.Shared.ApiInfrastructure;
 
 namespace TransportCompany.Driver.WebAPI
@@ -25,6 +29,21 @@ namespace TransportCompany.Driver.WebAPI
 
             var dbContext = services.BuildServiceProvider().GetService<DriverDbContext>();
             dbContext.Database.Migrate();
+        }
+
+        protected override void ConfigureApplicationLayerServices(IServiceCollection services)
+        {
+            var applicationLayerAssembly = Assembly.Load("TransportCompany.Order.Application");
+            RegisterAllServicesScopedFromAssembly(services, applicationLayerAssembly);
+
+            services.AddMediatR(applicationLayerAssembly);
+            //services.AddAutoMapper(applicationLayerAssembly);
+        }
+
+        protected override void ConfigureInfrastructureLayerServices(IServiceCollection services)
+        {
+            var infrastructureLayerAssembly = Assembly.GetAssembly(typeof(OrderDbContext));
+            RegisterAllServicesScopedFromAssembly(services, infrastructureLayerAssembly);
         }
     }
 }
